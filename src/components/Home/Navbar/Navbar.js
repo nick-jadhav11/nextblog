a6a6c17d9065851c48ffccdc99acec8780a3dc75
Sagar Bhise <sagarbhise@Sagars-MacBook-Air.local>
2025-06-09 15:48:18 +0530

    logo updated

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 'use client'
 import { navLinks } from '@/constant/Constant'
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { HiBars3BottomRight } from 'react-icons/hi2'
@@ -23,7 +24,8 @@ const Navbar = ({openNav}) => {
     <div className={` fixed ${navBg ? 'bg-white shadow-md' : 'fixed'} w-full transition-all duration-200 h-[12vh] z-[999]`}>
         <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
             {/* LOGO */}
-            <h1 className='text-xl md:text-2xl font-bold'><span className='text-3xl md:text-4xl text-pink-700'>N</span>avbar</h1>
+            {/* <h1 className='text-xl md:text-2xl font-bold'><span className='text-3xl md:text-4xl text-pink-700'>N</span>avbar</h1> */}
+            <Image src="images/logo.png" alt='LOGO' width={75} height={75} className=''/>
             {/* NavLinks */}
             <div className='hidden lg:flex items-center space-x-10'>
                 { navLinks.map((link) => {
